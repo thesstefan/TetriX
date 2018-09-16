eb8cbadb6f14237769e202d858d9618aab9826c8
TheMightyStefan <stefanthefineman@gmail.com>
2018-09-16 10:43:23 +0300

    Various modifications to Piece

--- src/piece.cpp
+++ src/piece.cpp
@@ -1,8 +1,7 @@
 #include "piece.h"
 
-Piece::Piece(const ofColor &color, const enum PieceShape shapeType,
-             const ofPoint &position) :
-    color(color), position(position), shape(getShapeFromType(shapeType)) {
+Piece::Piece(const ofPoint &position, const enum PieceShape shapeType) :
+    position(position), color(getColorFromType(shapeType)), shape(getShapeFromType(shapeType)) {
     
     currentRotation = 0;
 }
@@ -10,15 +9,9 @@ Piece::Piece(const ofColor &color, const enum PieceShape shapeType,
 void Piece::drawTile(const ofPoint &position) const {
     ofPushStyle();
 
-    ofSetColor(ofColor::black);
-
-    const float CONTOUR_SIZE = TILE_SIZE / 10;
-
-    ofDrawRectangle(position - CONTOUR_SIZE, TILE_SIZE + CONTOUR_SIZE, TILE_SIZE + CONTOUR_SIZE);
-
     ofSetColor(color);
 
-    ofDrawRectangle(position, TILE_SIZE, TILE_SIZE);
+    ofDrawRectangle(position, TILE_SIZE - 0.5, TILE_SIZE - 0.5);
 
     ofPopStyle();
 }
@@ -31,12 +24,34 @@ void Piece::draw() const {
 
         for (auto const &column : row) {
             if (column == true)
-                drawTile(ofPoint(position.x + TILE_SIZE * row_index,
-                                 position.y + TILE_SIZE * column_index));
+                drawTile(ofPoint(position.x + TILE_SIZE * column_index,
+                                 position.y + TILE_SIZE * row_index));
 
             column_index++;
         }
 
         row_index++;
     }
+}
+
+void Piece::rotate(int direction) {
+    if (direction < 0) {
+        if (currentRotation == 3)
+            currentRotation = 0;
+        else 
+            currentRotation++;
+    } else if (direction > 0) {
+        if (currentRotation == 0)
+            currentRotation = 3;
+        else 
+            currentRotation--;
+    }
+}
+
+void Piece::translate(const ofPoint &amount) {
+    position += amount;
+}
+
+ofPoint Piece::getPosition() const {
+    return position;
 }
--- src/piece.h
+++ src/piece.h
@@ -1,17 +1,23 @@
+#pragma once
+
 #include "ofMain.h"
 
-enum PieceShape { SquareShape, LShape };
+const int TILE_SIZE = 20;
+
+enum PieceShape { OShape, TShape, IShape, JShape, LShape, SShape, ZShape };
+
+ofColor getColorFromType(const enum PieceShape shapeType);
 
 std::array<std::array<std::array<bool, 4>, 4>, 4>
     getShapeFromType(const enum PieceShape shapeType);
 
 class Piece {
     private:
-        constexpr static int TILE_SIZE = 20;
+        ofPoint position;
+
+        ofRectangle bounds;
 
         const ofColor color;
-
-        ofPoint position;
 
         std::array<std::array<std::array<bool, 4>, 4>, 4> shape;
 
@@ -20,8 +26,13 @@ class Piece {
         int currentRotation;
 
     public:
-        Piece(const ofColor &color, const enum PieceShape shapeType,
-              const ofPoint &position);
+        Piece(const ofPoint &position, const enum PieceShape shapeType);
 
         void draw() const;
+
+        void rotate(int direction);
+
+        void translate(const ofPoint &amount);
+
+        ofPoint getPosition() const;
 };
