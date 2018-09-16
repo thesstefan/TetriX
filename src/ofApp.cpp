d95ff6ee8a91555faed6849e1ee603a4bd6379ca
TheMightyStefan <stefanthefineman@gmail.com>
2018-09-16 10:43:56 +0300

    Update ofApp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,72 +1,40 @@
 #include "ofApp.h"
 
+#include <iostream>
+
+ofApp::ofApp() : board(ofRectangle(ofPoint(ofGetWidth() / 3, 100), TILE_SIZE * 20, TILE_SIZE * 30)) {}
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofBackground(0, 0, 0);
+
     piece = std::unique_ptr<Piece>
-        (new Piece(ofColor::red, PieceShape::SquareShape, ofPoint(100, 100)));
+        (new Piece(ofPoint(ofGetWidth() / 3, 100), PieceShape::IShape));
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    float newTime = ofGetElapsedTimef() - deltaTime;
 
+    if (newTime >= 0.5) {
+        piece->translate(ofPoint(0, TILE_SIZE));
+
+        deltaTime += newTime;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     piece->draw();
+
+    board.draw();
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::keyPressed(int key) {
+    if (key == OF_KEY_RIGHT)
+        piece->translate(ofPoint(TILE_SIZE, 0));
+    else if (key == OF_KEY_LEFT)
+        piece->translate(ofPoint(-TILE_SIZE, 0));
+    else if (key == OF_KEY_UP)
+        piece->rotate(1);
 }
